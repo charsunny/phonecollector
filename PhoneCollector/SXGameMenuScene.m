82a0156a83cddbb7f3c725695234cb39e44bb88c
lanstonpeng <lanstonpeng@gmail.com>
2014-08-27 15:01:22 +0800

    add other button

--- PhoneCollector/SXGameMenuScene.m
+++ PhoneCollector/SXGameMenuScene.m
@@ -10,11 +10,19 @@
 #import "SXGameScene.h"
 
 
+#define TRANSITION_TIME_INTERVAL 0.3
+
 @interface SXGameMenuScene()
 
 @property (strong,nonatomic)SKTransition* sceneTransition;
 
 @property (strong,nonatomic)UIButton* startBtn;
+
+@property (strong,nonatomic)UIButton* gameCenterBtn;
+
+@property (strong,nonatomic)UIButton* settingBtn;
+
+@property (strong,nonatomic)UIView* gameLogoView;
 @end
 
 @implementation SXGameMenuScene
@@ -39,11 +47,35 @@
     _startBtn.backgroundColor = [UIColor blueColor];
     [_startBtn addTarget:self action:@selector(transferToMainGameScene:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_startBtn];
+    
+    _gameCenterBtn = [[UIButton alloc]initWithFrame:CGRectMake(f.size.width / 3 - 40 , f.size.height - 20 , 80, 20)];
+    [_gameCenterBtn setTitle:@"Game Center" forState:UIControlStateNormal];
+    _gameCenterBtn.titleLabel.adjustsFontSizeToFitWidth = YES;
+    _gameCenterBtn.backgroundColor = [UIColor blueColor];
+    [_gameCenterBtn addTarget:self action:@selector(handleGameCenterTap:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_gameCenterBtn];
+    
+    _settingBtn = [[UIButton alloc]initWithFrame:CGRectMake(f.size.width / 3 * 2 - 40 , f.size.height - 20 , 80, 20)];
+    [_settingBtn setTitle:@"Setting" forState:UIControlStateNormal];
+    _settingBtn.titleLabel.adjustsFontSizeToFitWidth = YES;
+    _settingBtn.backgroundColor = [UIColor blueColor];
+    [_settingBtn addTarget:self action:@selector(handleSetting:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_settingBtn];
+}
+
+- (void)handleSetting:(id)sender
+{
+    
+}
+
+- (void)handleGameCenterTap:(id)sender
+{
+    
 }
 
 - (void)transferToMainGameScene:(id)sender
 {
-    _sceneTransition = [SKTransition doorsOpenVerticalWithDuration:0.3];
+    _sceneTransition = [SKTransition doorsOpenVerticalWithDuration:TRANSITION_TIME_INTERVAL];
     
     SKScene * scene = [SXGameScene sceneWithSize:self.frame.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
@@ -56,6 +88,8 @@
 - (void)removeUIKitView
 {
     [_startBtn removeFromSuperview];
+    [_gameCenterBtn removeFromSuperview];
+    [_settingBtn removeFromSuperview];
 }
 
 @end
